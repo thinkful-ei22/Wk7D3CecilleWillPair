1e8d95bfd23e99decfd34e0f435b175939fc04a2
Cecille Salazar <cecillersalazar@gmail.com>
2018-07-25 11:56:30 -0700

    Bugs fixed

--- src/components/game.js
+++ src/components/game.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import InputBox from './inputbox';
 import OutputBox from './outputbox';
 
@@ -13,24 +12,21 @@ export default class Game extends React.Component {
       userInput: ''
     }
     this.onSubmit = this.onSubmit.bind(this)
-    this.onInputChange = this.onInputChange.bind(this)
   }
 
-  onInputChange(event) {
-    this.setState({
-      userInput: event.target.value
-    })
-  }
-  onSubmit(event){
+  onSubmit(event) {
     event.preventDefault()
-    this.setState({ draw: true })
+    this.setState({
+      draw: true,
+      userInput: event.target.inputbox.value
+    })
   }
 
   render() {
     return (
       <div>
         <p>{this.props.title}</p>
-        <InputBox onSubmit={this.onSubmit} onInputChange={this.onInputChange}/>
+        <InputBox onSubmit={this.onSubmit} />
         {this.state.draw && <OutputBox />}
       </div>
     )
--- src/components/outputbox.js
+++ src/components/outputbox.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import ReactDOM from 'react-dom';
-
 export default function OutputBox(props) {
   const computerChoice = Math.floor(Math.random() * 3) + 1;
     let output;
